5d91bfe8e16568fcb73423377f58241c12b64c4b
Honeyoceanmtech <142771911+Honeyoceanmtech@users.noreply.github.com>
2023-09-01 11:21:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask_cors import CORS
+from flask import Flask , request,jsonify
+from pymongo import MongoClient
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    values = "Hello"
+    return jsonify(values)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
